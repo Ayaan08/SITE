ff0670974525097905f9e9ce7d091c42c055e00a
Ayaan08 <85597340+Ayaan08@users.noreply.github.com>
2024-02-13 06:17:22 +0530

    Update template.tsx

--- app/template.tsx
+++ app/template.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 export default function Template({ children }: { children: ReactNode }) {
     return (
         <main>
+            <script data-cfasync="false" src="//d1crfzlys5jsn1.cloudfront.net/?zfrcd=1024376"></script>
             <Navbar />
             <div className="md:mx-16 lg:mx-24 xl:mx-28 min-h-screen">
                 {children}
